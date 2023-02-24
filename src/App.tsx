729b9833ad3ce2c6162d27e20892d490effe1841
Lucas Araujo <lucasaraujo4188@gmail.com>
2023-02-24 18:47:09 -0300

    style: configurando eslint

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,12 @@ import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 export function App() {
-
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant='primary' />
-      <Button variant='secondary' />
-      <Button variant='success' />
-      <Button variant='danger' />
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
       <Button />
 
       <GlobalStyle />
